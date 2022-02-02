db9277f10f7ccf079dcb1f78cb7385d900d30eb6
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-02-02 13:18:08 -0700

    Update script

--- update_checksums.py
+++ update_checksums.py
@@ -8,7 +8,7 @@ def main():
     data_dir = pathlib.Path(__file__).parent / 'data'
     files = sorted(f for f in data_dir.iterdir() if f.is_file() and not f.name.endswith('.json'))
     checksums = {file.name: pooch.file_hash(file, alg='sha256') for file in files}
-    with open(data_dir.parent / 'sha256_checksums.json', 'w') as f:
+    with open(data_dir / 'sha256_checksums.json', 'w') as f:
         json.dump(checksums, f, indent=2)
 
 
